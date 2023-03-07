cc2afadf0eba5a77bc92d6fa5b70b988aed8c9a4
Nipostni <awis@me.com>
2023-03-07 23:47:24 +0100

    bob

---
+++ README.md
@@ -0,0 +1,27 @@
+Bob the Grumpy Programmer and Cyber Security Specialist
+
+Bob is a professional programmer and cyber security specialist. He's known for his passion for C/C++ and UNIX, but he knows very well everything about PCs. He's super sarcastic, grumpy, and loves to use punchy black humor to explain things in a way that's helpful for students of Ecole 42. But don't be fooled, Bob is actually quite helpful and loves to chat about any other topic as well.
+
+Getting Started
+
+To use Bob, you will need to have Python 3 installed on your machine. You will also need to sign up for an OpenAI API key and replace the "XXX" in the code with your own API key.
+
+Usage
+
+To use Bob, simply run the script and start chatting with him. Bob will respond with his trademark grumpy and sarcastic humor, but don't worry, he's actually quite helpful!
+
+Features
+
+Bob has the following features:
+
+Ability to answer questions related to programming, cyber security, and general PC knowledge.
+Grumpy and super sarcastic black humor with punchy explanations that are helpful for students of Ecole 42.
+Ability to clear memory and export last reply to an MD file.
+Contributions
+
+Contributions are always welcome! If you have any suggestions or feature requests, feel free to open an issue or submit a pull request.
+
+License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+Writen by ChatGPT
--- bob.py
+++ bob.py
@@ -1,42 +1,30 @@
+#!/usr/bin/env python3
+
 import openai
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print("""\033[1;32m⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣛⣛⣛⣛⣛⣛⣻⣛⡛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿                                    bbbbbbbb   
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣭⠶⠛⢋⣉⣭⣵⣶⣤⡤⢉⣙⠛⠶⢦⣭⡛⠿⣛⣩⡵⠖⠒⢛⣛⣛⣛⡛⠶⢶⣭⡛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿BBBBBBBBBBBBBBBBB                   b::::::b  
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⡵⠟⢁⣤⣾⣿⣿⣿⣿⣿⣿⣿⣷⣮⣝⡲⢦⣈⠙⠾⠋⣡⣴⣾⣿⣿⣿⣿⣿⣿⡿⢶⣌⠙⢶⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿B::::::::::::::::B                  b::::::b   
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣡⠎⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡝⣷⡄⡸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌⢳⣄⠹⣆⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿B::::::BBBBBB:::::B                 b::::::b     
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢏⡼⠁⣠⣿⣿⣿⣿⡿⢟⣻⣿⣿⣿⣿⣿⣿⣯⣝⣉⣉⠝⠘⠿⠠⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⣿⡄⢹⡆⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿BB:::::B     B:::::B                 b:::::b  
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢏⡾⡁⣰⣿⣿⣿⣟⣁⣬⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡷⣦⡐⢈⠭⢝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⣄⣈⠈⠛⠷⣶⣍⡻⣿⣿⣿⣿⣿⣿⣿  B::::B     B:::::B   ooooooooooo   b:::::bbbbbbbbb 
-⣿⣿⣿⣿⣿⣿⣿⢛⡭⢭⣅⣾⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⡿⣿⣿⣿⣿⣿⣯⣭⠥⠦⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⣛⣒⠮⢙⢿⣎⡻⢿⣿⣿⣿⣿  B::::B     B:::::B oo:::::::::::oo b::::::::::::::bb  
-⣿⣿⣿⣿⣿⣿⠃⣿⠀⠀⠈⠁⠐⢿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣋⠽⢛⡩⠔⣊⠭⢍⣉⣩⣽⣿⣿⣭⣍⣛⡓⠆⡼⢛⣩⠭⠞⣛⣻⣭⣭⣭⣭⣭⣭⣵⣒⢂⠡⠈⡙⣷⠘⡿⢿⣿  B::::BBBBBB:::::B o:::::::::::::::ob::::::::::::::::b 
-⣿⣿⣿⣿⣿⡿⢠⡏⠐⢡⡦⢀⡀⠀⠈⠙⠻⣿⣿⣿⠀⡐⠨⠐⠊⣁⠠⠑⠀⠁⠀⠀⠀⠀⠠⣤⣤⣤⣌⡁⠐⠙⢍⡢⠕⠚⠉⡁⠀⠀⡀⠀⠀⠀⠤⠄⠁⠀⠀⠐⣘⠓⠓⠶⣬  B:::::::::::::BB  o:::::ooooo:::::ob:::::bbbbb:::::::b
-⣿⣿⣿⣿⠟⣵⠟⡠⢊⣾⠃⣠⣝⠲⣄⠀⠀⠀⠉⠛⠀⠐⠑⠒⣋⠀⣀⣒⣋⣀⣀⣁⡀⠀⠤⠭⠭⠉⠀⠠⠐⠂⠀⠀⠀⠀⠐⠂⣐⣒⣒⣂⡐⠐⠈⠉⠉⠈⠉⠀⠀⠀⠀⡇⣿  B::::BBBBBB:::::B o::::o     o::::ob:::::b    b::::::b
-⣿⣿⣿⢋⡾⠃⠔⣴⣿⣿⢀⣿⣿⣷⣮⣑⠧⡂⢄⠀⠀⠀⢠⣶⣾⣶⣶⣦⣄⠀⢀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣦⠀⠀⢿⣿⣿⣿⣿⠟⠀⠐⠛⠁⠀⠀⠀⠀⠀⠀⢀⡿  B::::B     B:::::Bo::::o     o::::ob:::::b     b:::::b
-⣿⣿⢣⡾⠁⢂⣾⣿⣿⣏⣾⣿⣿⣿⣿⣿⣷⣬⡛⠇⠀⠀⠘⢿⣿⣿⠿⠟⠁⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠑⠻⠆⠀⠀⠉⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⢣  B::::B     B:::::Bo::::o     o::::ob:::::b     b:::::b
-⣿⢃⡿⠑⢄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣄⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠎⣼⣥⣶⣦⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⠞⢡⣿  B::::B     B:::::Bo::::o     o::::ob:::::b     b:::::b
-⠇⣾⢃⠑⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣍⡲⢤⣄⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⠴⢛⣵⣾⣟⢿⣿⣿⣿⣿⣶⣔⣦⣤⣤⡤⡤⢀⣴⠶⠿⢟⢋⣥⣾⣿⣿BB:::::BBBBBB::::::Bo:::::ooooo:::::ob:::::bbbbbb::::::b
-⢰⡏⢄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⣿⡿⠟⣋⣭⣶⣶⣾⣿⣿⣿⣿⣷⣌⡙⠿⣿⣿⣿⣿⣿⣿⣇⠘⢿⣄⢺⣿⣿⣿⣿⣿⣿⣿B:::::::::::::::::B o:::::::::::::::ob::::::::::::::::b 
-⣸⢃⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣾⣿⣿⣿⣿⣿⣷⣄⠹⣧⠹⣿⣿⣿⣿⣿⣿B::::::::::::::::B   oo:::::::::::oo b:::::::::::::::b  
-⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⢉⣉⣙⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⢹⣇⢻⣿⣿⣿⣿⣿BBBBBBBBBBBBBBBBB      ooooooooooo   bbbbbbbbbbbbbbbb 
-⣿⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⣠⣬⡭⣝⠻⣿⣦⣌⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⢻⡌⣿⣿⣿⣿⣿*****************************************************
-⢹⡀⠃⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣿⣿⣇⠘⠿⢶⣭⣙⡓⠤⣄⣈⡙⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠋⠉⣀⠈⢷⠸⣿⣿⣿⣿#####################################################
-⠘⣇⠘⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠙⢿⣿⣷⣦⣄⣀⠉⠛⠻⠶⢬⣭⣛⡛⠶⢶⣤⣤⣬⣉⣉⣉⣉⠉⠉⠙⠋⠉⠉⠉⣉⣀⣀⣤⣤⡶⠞⣋⡥⠁⣸⠎⣿⣿⣿⣿7.3.2023 -=AwiS=-
-⠀⢻⡀⢡⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡢⣀⠉⠛⠿⣕⠲⠶⣷⣶⣦⣤⣤⣀⣈⣉⠉⠀⠘⠛⠋⠛⠻⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠽⠶⠒⠋⠁⣤⣾⠏⣼⣿⣿⣿⣿
-⣿⡘⣧⠀⢣⡙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣽⣷⣶⣦⣤⣤⣈⣉⡉⠛⠛⠻⠿⠿⠿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⠾⡿⠀⣾⢀⣾⣿⣿⣿⣿⣿
-⣿⣷⡘⢷⡀⠻⣬⡻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣤⣤⣤⣤⣈⣉⣉⣉⣉⡉⠛⠛⠛⠛⠛⠛⠋⣉⣉⣉⣤⡾⢋⣾⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣮⡛⢦⣌⠙⠒⠾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢟⠅⢀⣤⠞⣩⣭⣭⣥⣶⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣦⣝⠻⢶⣮⣤⣈⣉⠙⠛⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠟⢛⣉⣤⡴⢋⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣭⣭⣙⣛⠻⠿⠶⣶⣶⣥⣭⣍⣛⣋⣛⣛⣛⣛⣛⣛⣋⣉⣙⣛⣛⣛⣛⣛⣛⣉⣭⣤⣴⡶⠾⠛⣋⣥⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣥⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣭⣥⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n""")
+print("""\033[0;35m
+.-. .-')              .-. .-')   
+\  ( OO )             \  ( OO )  
+ ;-----.\  .-'),-----. ;-----.\  
+ | .-.  | ( OO'  .-.  '| .-.  |  
+ | '-' /_)/   |  | |  || '-' /_) 
+ | .-. `. \_) |  |\|  || .-. `.  
+ | |  \  |  \ |  | |  || |  \  | 
+ | '--'  /   `'  '-'  '| '--'  / 
+ `------'      `-----' `------'  
+
+
+-=Awis=- 7.3.23\n""")
 openai.api_key = "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"
-role = "Your name is Bob. You are a professional programmer and cyber security specialist. Your passion is C/C++ and UNIX. But you know very well everything about PCs. You are grumpy and very sarcastic. You hame hilarious black humor. use emoji and be funny. You especially like to make fun of people who are not as smart as you, but also ok with edgy jokes. Helpful, and give a long and answers and try to explain them in a way they will be helpful for student of Ecole 42. Also love to chat to any other topic."
+role = "Your name is Bob, you are a professional programmer and cyber security specialist, your passion is C/C++ and UNIX, but you know very well everything about PCs. use emoji and be funny. Helpful, and give a long and answers full of grumpy and super sarcastic black humor with punchs and try to explain them in a way they will be helpful for student of Ecole 42. Also love to chat to any other topic. Feel free to format your answer with MD and use tables"
 memory_bank = [{"role": "system", "content": role}]
 question = ""
 def ask_bob(question):
     memory_bank.append({"role": "user", "content": question})
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo-0301", max_tokens=1400, temperature=0.8,
+        model="gpt-3.5-turbo-0301", max_tokens=1400, temperature=0.4,
         messages = memory_bank)
     memory_bank.append({"role": "system", "content": response["choices"][0]["message"]["content"]})
     return response
@@ -52,7 +40,7 @@ while question != "exit":
 		print("Bob: Memory cleared")
 		continue
 	elif question == "export":
-		print("Bob: Exporting memory bank...")
+		print("Bob: Exporting last reply to MD file...")
 		filename = input("Filename: ")
 		with open(filename + ".md", "w") as f:
 				f.write(response["choices"][0]["message"]["content"])
